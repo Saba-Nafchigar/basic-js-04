f670ca327e5aeb2830d7cd7eb36a5dd7be350c8c
Saba <sabanafchigar@gmail.com>
2023-02-11 16:18:33 +0330

    Add files via upload

---
+++ Upper-triangular-matrix.js
@@ -0,0 +1,12 @@
+let x = Number(prompt("x...")) // 5
+    for(let i = 1 ; i <= x ; i++){ 
+        for(let j = 1 ; j <= x ; j++){ 
+            if(i<=j){
+                document.write(`<span>1</span>`)
+            }else{
+                document.write(`<span>0</span>`) 
+            }
+            
+        }
+        document.write(`<br/>`)
+    }
