a108d751ad9931c61c732e3aad45323e61bb14e1
Saba <sabanafchigar@gmail.com>
2023-02-11 15:31:09 +0330

    Delete 2-dimensional-matrix.js

--- 2-dimensional-matrix.js
+++
@@ -1,7 +0,0 @@
-let x = Number(prompt("x..."))
-    for(let i = 1 ; i <= x ; i++){
-        for(let j = 1 ; j <= x ; j++){
-          document.write(`<span>${j}</span>`)   
-        }
-        document.write(`<br/>`)
-    }
